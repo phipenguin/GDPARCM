33861335a034f9a245f96cf7d91c6a222bdae04e
phipenguu <paul_ong@dlsu.edu.ph>
2023-03-19 08:24:33 +0800

    Added preliminary audio

---
+++ StreamSoundLoader.cpp
@@ -0,0 +1,29 @@
+﻿#include "StreamSoundLoader.h"
+#include "AudioManager.h"
+#include "StringUtils.h"
+#include "IExecutionEvent.h"
+#include <iostream>
+
+StreamSoundLoader::StreamSoundLoader(String path, IExecutionEvent* execution_event)
+{
+	this->path = path;
+	this->exec_event = execution_event;
+}
+
+StreamSoundLoader::~StreamSoundLoader()
+{
+}
+
+void StreamSoundLoader::onStartTask()
+{
+	Thread::sleep(5000);
+
+	std::vector<String> tokens = StringUtils::split(path, '/');
+	String asset_name = StringUtils::split(tokens[tokens.size() - 1], '.')[0];
+	AudioManager::getInstance()->instantiateAsSound(path, asset_name);
+
+	std::cout << "[AudioManager] Loaded streaming sound: " << asset_name << std::endl;
+
+	this->exec_event->onFinishedExecution();
+	delete this;
+}
---
+++ StreamSoundLoader.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Thread.h"
+#include "IWorkerAction.h"
+
+class IExecutionEvent;
+class StreamSoundLoader : public IWorkerAction
+{
+private:
+	typedef std::string String;
+
+public:
+	StreamSoundLoader(String path, IExecutionEvent* execution_event);
+	~StreamSoundLoader();
+
+private:
+	String path;
+	IExecutionEvent* exec_event;
+
+	void onStartTask() override;
+};
